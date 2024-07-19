a2a7a397bcef97405448cf98e6cdd8c4d3256d4b
Chandler Abril <abrilchandler1@gmail.com>
2024-07-19 15:47:58 -0700

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, mode="w") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, file_content):
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, mode="a") as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = f"{file_name}.txt"
+    with open(file_name_with_extension, mode="r") as file:
+        content = file.read()
+    return content
